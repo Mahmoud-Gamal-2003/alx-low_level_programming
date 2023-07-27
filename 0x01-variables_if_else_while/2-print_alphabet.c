91e5e360e197eeed1209bcd15cf3f3b6b93bef54
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-07-26 18:27:10 -0700

    for1:
    dckkdld

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print a random number.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 97; i <= 122; i++)
+putchar(i);
+return (0);
+}
