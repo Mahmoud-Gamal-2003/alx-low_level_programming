00df50779b020773ee85d4430269cdff55e79595
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-07-26 17:24:50 -0700

    advansed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+ * main - Entry point
+ * Description: print the text with out use printf.
+ * Return: Always 1 (Filled)
+ */
+int main(void)
+{
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  return (1);
+}
