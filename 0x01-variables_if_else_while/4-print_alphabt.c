73e09c511049e59b489e56ece5a78d61834e53ba
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-07-26 20:57:10 -0700

    exptqe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lower exept e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'a';
+/*print a - z*/
+while (ch <= 'z')
+{
+ch++;
+if (ch == 'e' || ch == 'q')
+{
+continue;
+}
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
