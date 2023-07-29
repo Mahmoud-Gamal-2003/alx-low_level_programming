5a22f05352e6b51f701762e0971941dec67d87a3
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-07-28 21:42:34 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function that transfar
+ * @n: prints _putchar using putchar prototype
+ *
+ * Return: lowercase 1 (Success), otherwise 0 (failed)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
