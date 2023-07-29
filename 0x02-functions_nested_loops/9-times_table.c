20f464a2691f0e38bcb805d366e1d833de05b137
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-07-29 02:38:14 -0700

    9times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - writes the character c to stdout
+ * Discreption: The character to print
+ *
+ * Return: on success 1.
+ *         on error, -1 is returned, and error is set appropriates.
+ */
+void times_table(void)
+{
+int i, j, k;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = i * j;
+if (j != 0 && k < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(',');
+}
+else if (k >= 10)
+{
+_putchar(' ');
+_putchar(',');
+}
+if (((k / 10) + 48) != 48)
+_putchar((k / 10) + 48);
+_putchar((k % 10) + 48);
+if (j == 9)
+{
+_putchar('$');
+}
+}
+_putchar('\n');
+}
+}
