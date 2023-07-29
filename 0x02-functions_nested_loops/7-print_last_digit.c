280ac4230e1f0acfbb9c1b0a4afdc765816ba20d
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-07-28 21:54:22 -0700

    7print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - The entry point
+ * @n: prints _putchar using putchar prototype
+ *
+ * Return: lowercase 1 (Success), otherwise 0 (failed)
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
