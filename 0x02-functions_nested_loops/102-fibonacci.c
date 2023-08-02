5216fcc7eb08465ec8ce3e45b2c3007761cd9ca8
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-02 02:41:29 -0700

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+unsigned long f1 = 0, f2 = 1, sum;
+for (i = 0; i < 50; i++)
+{
+sum = f1 + f2;
+printf("%lu", sum);
+f1 = f2;
+f2 = sum;
+if (i == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
