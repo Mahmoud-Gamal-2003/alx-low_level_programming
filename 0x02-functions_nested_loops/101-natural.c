7b3c43f0f05205bf7c1da95373ae0a9d0399a06d
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-02 02:05:20 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int result = 0;
+int i;
+
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+result += i;
+}
+printf("%d", result);
+return (0);
+}
