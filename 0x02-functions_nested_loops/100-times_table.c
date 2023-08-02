cebc830154e2026c8da86780335fee5a496c4424
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-02 01:15:25 -0700

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_times_table - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: on success 1.
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (i * j < 10)
+				{
+					if (j != 0)
+						printf("   ");
+					printf("%d", j * i);
+				}
+				else if (i * j >= 10 && i * j < 100)
+				{
+					if (j != 0)
+						printf("  ");
+					printf("%d", j * i);
+				}
+				else if (i * j >= 100)
+				{
+					if (j != 0)
+						printf(" ");
+					printf("%d", j * i);
+				}
+			}
+			printf("\n");
+		}
+	}
+}
