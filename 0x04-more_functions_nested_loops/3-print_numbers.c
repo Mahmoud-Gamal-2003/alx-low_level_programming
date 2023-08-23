fce5289834caa6bf92fc5ae54bf1939d6bb8c212
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-23 11:47:37 -0700

    helo

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,4 +11,4 @@ void print_numbers(void)
 	for (i = 48; i <= 57; i++)
 		_putchar(i);
 	_putchar('\n');
-
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isupper - cheack if the char upper case or lower
+ *
+ * @c: the ASCII value of the char
+ *
+ * Return: return one if true and zero if false
+*/
+
