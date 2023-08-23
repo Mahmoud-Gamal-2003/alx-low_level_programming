238bb52a9cc8543b75405d5f5446f4d042a7b170
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-23 12:13:27 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,20 @@
 #include "main.h"
 /**
- * _isupper - cheack if the char upper case or lower
- *
- * @c: the ASCII value of the char
+ * print_most_numbers - cheack if the char upper case or lower
  *
  * Return: return one if true and zero if false
 */
+void print_most_numbers(void)
+{
+	int i;
 
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
