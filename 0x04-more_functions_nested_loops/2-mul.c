512687169c3915410b1ac690beca031130ffd1a7
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-23 11:39:13 -0700

    hel

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - cheack if the char upper case or lower
+ *
+ * @a: the ASCII value of the char
+ *
+ * @b: the sec num
+ * Return: return one if true and zero if false
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
