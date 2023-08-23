48fdccc73285387012a04d3811fa744f2c233222
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-23 11:45:00 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - cheack if the char upper case or lower
+ *
+ * Return: return one if true and zero if false
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+
