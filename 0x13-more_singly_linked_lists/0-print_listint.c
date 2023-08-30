d6f29cf0c60da6d53f130e23099813ccedfa73e1
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-29 17:22:36 -0700

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * _strlen - return the
+ * @s: the sting whose3 length to check
+ *
+ * Return: integer length of sting
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - print
+ * @h: pointer to first
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
