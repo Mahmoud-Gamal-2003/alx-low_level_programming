3bb4d0a5fb493ddd2e201aa5126fd92fc7b1dbed
Mahmoud-Gamal-2003 <memomemo1152003@gmail.com>
2023-08-29 18:11:51 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,35 +1,18 @@
 #include "lists.h"
 
 /**
- * _strlen - return the
- * @s: the sting whose3 length to check
+ * print_listint - return the
+ * @h: the sting whose3 length to check
  *
  * Return: integer length of sting
  */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * print_list - print
- * @h: pointer to first
- *
- * Return: size of list
- */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,24 +6,22 @@
 #include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (mal)
- * @len: length of the string
- * @next: pointer
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: singly or
- * for Holberton project
+ * Description: singly linked list node structure
+ *
  */
-typedef struct list_s
+typedef struct listint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-}list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 int _putchar(char c);
-int _strlen(char *s);
-size_t print_list(const list_t *h);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
@@ -36,6 +34,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
